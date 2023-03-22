9c46902bbc037f70101c3ce47e6ccf2d3c35be3a
anna carolina <c.anna@aluno.ifsp.edu.br>
2023-03-22 18:35:36 -0300

    exercicio 02 semana 04

--- src/semana04/Ex01.java
+++ src/semana04/Ex01.java
@@ -4,14 +4,22 @@ import javax.swing.JOptionPane;
 
 public class Ex01 {
     public static void main(String[] args) {
-        int numInteiro, sucessor, antecessor;
-        numInteiro = Integer.parseInt(JOptionPane.showInputDialog("Insira um número: "));
+        Scanner sc = new Scanner(System.in);
 
-        sucessor = numInteiro + 1;
-        antecessor = numInteiro -1;
+        System.out.println("Insira o primeiro numero inteiro: ");
+        double primeiro = sc.nextDouble(); 
 
-        System.out.println("Sucessor: " + sucessor);
-        System.out.println("Antecessor:" + antecessor);
+        System.out.println("Insira o segundo numero inteiro: ");
+        double segundo = sc.nextDouble(); 
+
+
+        System.out.println("Insira o terceiro numero inteiro: ");
+        double terceiro = sc.nextDouble(); 
+        
+
+        double mediaAritmetica = (primeiro+segundo+terceiro)/3;
+
+        System.out.println("A media aritmetica dos numeros é: " + mediaAritmetica);
 
        
 
